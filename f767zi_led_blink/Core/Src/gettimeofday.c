1f32c24e05ec118e1e0a5ae8fc0aaa3ff78a8936
Rishi Latchmepersad <latchmepesad@gmail.com>
2025-05-05 20:13:02 -0400

    Initial code for blinking an LED

---
+++ f767zi_led_blink/Core/Src/gettimeofday.c
@@ -0,0 +1,14 @@
+/*
+ * gettimeofday.c
+ *
+ *  Created on: May 5, 2025
+ *      Author: rishi_latchmepersad
+ */
+
+#include <sys/time.h>
+
+int _gettimeofday( struct timeval *tv, void *tzvp )
+{
+    // you can add code here there many example in google search.
+    return 0;  // return non-zero for error
+} // end _gettimeofday()
